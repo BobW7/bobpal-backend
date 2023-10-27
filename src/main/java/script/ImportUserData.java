38734871f860376769e817b6b3cce6f4154a51db
BobW7 <1186717311@qq.com>
2023-10-27 16:46:09 +0800

    过滤重复用户信息

---
+++ src/main/java/script/ImportUserData.java
@@ -0,0 +1,26 @@
+package script;
+
+import com.alibaba.excel.EasyExcel;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 导入用户数据到数据库
+ */
+public class ImportUserData {
+    public static void main(String[] args) {
+        String fileName ="D:\\planetProject\\bobpal-backend\\src\\main\\resources\\UserData.xls";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 同步读取会自动finish
+        List<TableMyUserInfo> userInfoList =
+                EasyExcel.read(fileName).head(TableMyUserInfo.class).sheet().doReadSync();
+        System.out.println("总数 = "+userInfoList.size());
+        //过滤
+        //首先将昵称相同的用户的信息放到同一组下
+        Map<String, List<TableMyUserInfo>> listMap =
+                userInfoList.stream().collect(Collectors.groupingBy(TableMyUserInfo::getUsername));
+        System.out.println("无重复的昵称数 = "+listMap.keySet().size());
+
+    }
+}
