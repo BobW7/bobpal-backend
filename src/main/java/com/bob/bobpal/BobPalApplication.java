8c0a12ca10e6044cc4c76fe80fe38df188d015a7
BobW7 <1186717311@qq.com>
2023-11-01 18:00:22 +0800

    开启定时任务

--- src/main/java/com/bob/bobpal/BobPalApplication.java
+++ src/main/java/com/bob/bobpal/BobPalApplication.java
@@ -3,9 +3,11 @@ package com.bob.bobpal;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.bob.bobpal.mapper")
+@EnableScheduling
 public class BobPalApplication {
 
     public static void main(String[] args) {
