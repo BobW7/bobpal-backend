0ce68a326f4c45483e2c43a90ade6b7a617b0618
BobW7 <1186717311@qq.com>
2023-11-06 17:19:55 +0800

    用Redisson实现分布式锁

--- src/main/java/com/bob/bobpal/job/PreCacheJob.java
+++ src/main/java/com/bob/bobpal/job/PreCacheJob.java
@@ -2,10 +2,11 @@ package com.bob.bobpal.job;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.bob.bobpal.mapper.UserMapper;
 import com.bob.bobpal.model.domain.User;
 import com.bob.bobpal.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -26,24 +27,43 @@ public class PreCacheJob {
     private UserService userService;
     @Resource
     private RedisTemplate<String,Object> redisTemplate;
+
+    @Resource
+    private RedissonClient redissonClient;
     //重点用户
     private List<Long> mainUserList = Arrays.asList(4L);
     //每天执行，预热推荐用户
     @Scheduled(cron = "0 58 23 * * *")
     public void doCacheRecommendUser(){
-        for (Long userId : mainUserList) {
-            //无缓存，走数据库
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            String redisKey = String.format("bobpal:user:recommend:%s",userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-            //写缓存
-            try {
-                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.error("redis set key error",e);
+        RLock lock = redissonClient.getLock("bobpal:precachejob:docache:lock");
+        try {
+            //只有一个线程能获取到锁
+            if(lock.tryLock(0,30000,TimeUnit.MILLISECONDS)){
+                System.out.println("getLock: " + Thread.currentThread().getId());
+                for (Long userId : mainUserList) {
+                    //无缓存，走数据库
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("bobpal:user:recommend:%s",userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    //写缓存
+                    try {
+                        valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e) {
+                        log.error("redis set key error",e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error",e);
+        }finally {
+            //判断当前锁是否属于当前线程，避免释放其它线程的锁
+            if(lock.isHeldByCurrentThread()){
+                System.out.println("unlock: "+Thread.currentThread().getId());
+                lock.unlock();
             }
         }
+
 
     }
 }
