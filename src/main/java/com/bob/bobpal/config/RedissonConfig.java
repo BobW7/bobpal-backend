6b2a7b8e6ba7d6041544d390a691819350668339
BobW7 <1186717311@qq.com>
2023-11-06 16:09:41 +0800

    优雅写配置，不写死IP

--- src/main/java/com/bob/bobpal/config/RedissonConfig.java
+++ src/main/java/com/bob/bobpal/config/RedissonConfig.java
@@ -1,8 +1,10 @@
 package com.bob.bobpal.config;
 
+import lombok.Data;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,16 +12,19 @@ import org.springframework.context.annotation.Configuration;
  * Redisson 配置
  */
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
 public class RedissonConfig {
+    private String host;
+    private String port;
     @Bean
     public RedissonClient redissonClient(){
         // 1. Create config object
         Config config = new Config();
-        String redisAddress = "redis://127.0.0.1:6379";
+        String redisAddress =String.format("redis://%s:%s",host,port);
         config.useSingleServer().setAddress(redisAddress).setDatabase(3);
 
         // 2. Create Redisson instance
-
         // Sync and Async API
         RedissonClient redisson = Redisson.create(config);
         return redisson;
